21b996f7032adbf0b2952c3d222b086c8774348f
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2025-09-01 04:19:06 -0700

    fix: untrack unnecessary files

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { TaskManager } from './task-manager';
-export type { Task, WorkspaceConfig, WorkspaceStatus } from './task-manager';
-export * from './task-manager';
-//# sourceMappingURL=index.d.ts.map
